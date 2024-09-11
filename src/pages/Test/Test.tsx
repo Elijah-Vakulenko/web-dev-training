65eef29be7338435c6f671a14a8e0646c2216523
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-09-11 20:04:01 +0100

    test2

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import GoHomeBtn from '../../components/GoHomeBtn/GoHomeBtn';
 
-const Test = () => {
+const Test: React.FC = () => {
 
-  
+  const planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+console.table(planets);
+const lastIndex = planets.length -1;
+
+
+for (let i = 0; i <= lastIndex; i += 1){
+console.log(planets[i]);
+}
+
 
     return (
   <div>
