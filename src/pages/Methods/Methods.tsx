ba6c74e1e61d7d1033cc03c8af8a4adc96ff4b95
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-09-17 20:07:03 +0100

    +

---
+++ src/pages/Methods/Methods.tsx
@@ -0,0 +1,9 @@
+import GoHomeBtn from '../../components/GoHomeBtn/GoHomeBtn';
+const Methods: React.FC = () => {
+  return (
+     <div className='container'>
+      <GoHomeBtn />+</div>
+  )
+}
+
+export default Methods
