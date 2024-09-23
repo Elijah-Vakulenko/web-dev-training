5aa76d7a2ec7afe6715c152824c510dc012b0307
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-09-23 23:23:55 +0100

    Update Methods.tsx

--- src/pages/Methods/Methods.tsx
+++ src/pages/Methods/Methods.tsx
@@ -1,8 +1,26 @@
 import GoHomeBtn from '../../components/GoHomeBtn/GoHomeBtn';
+
+const str = `
+const x = 10;
+
+String(x) // ← перетворення у рядок
+x.toString() // ← перетворення у рядок
+`
+
 const Methods: React.FC = () => {
+
+
+
+
+
   return (
      <div className='container'>
-      <GoHomeBtn />+</div>
+      <GoHomeBtn />
+      <div className='input'>
+        <h3>Перетворення у рядок</h3>
+        <pre>{str }</pre>
+    </div>
+    </div>
   )
 }
 
