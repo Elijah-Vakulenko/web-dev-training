6b4dbbd83031c0b05152586d0f51e4999c6f51b0
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-09-25 12:23:31 +0100

    method num

--- src/pages/Methods/Methods.tsx
+++ src/pages/Methods/Methods.tsx
@@ -7,6 +7,11 @@ String(x) // ← перетворення у рядок
 x.toString() // ← перетворення у рядок
 `
 
+const num = `
+const y = '10';
+Number(y) //← перетворення у число
++y // ← перетворення у число за допомогою унарного плюсу `
+
 const Methods: React.FC = () => {
 
 
@@ -16,10 +21,18 @@ const Methods: React.FC = () => {
   return (
      <div className='container'>
       <GoHomeBtn />
-      <div className='input'>
-        <h3>Перетворення у рядок</h3>
-        <pre>{str }</pre>
-    </div>
+     <div className='wrapper'>
+        <div className='input'>
+          <h3>Перетворення у рядок</h3>
+          <pre>{str }</pre>
+        </div>
+        <div className='input'>
+          <h3>Перетворення у число</h3>
+          <pre>{num}</pre>
+        </div>
+     </div>
+      
+
     </div>
   )
 }
