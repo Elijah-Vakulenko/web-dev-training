30698d667b01d9c6ea9d9e189551a172910722f6
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-10-20 20:24:32 +0100

    masonry

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { DNA } from 'react-loader-spinner'
+import { DNA } from 'react-loader-spinner';
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const Arrays = lazy(() => import('./pages/Arrays/Arrays'));
@@ -18,44 +18,38 @@ const Galleries = lazy(() => import('./pages/Galleries/Galleries'));
 const Cycles = lazy(() => import('./pages/Cycles/Cycles'));
 const BasicStatements = lazy(() => import('./pages/BasicStatements/BasicStatements'));
 const BranchStatements = lazy(() => import('./pages/BranchStatements/BranchStatements'));
-const Methods = lazy(() => import('./pages/Methods/Methods')) 
-const Operators = lazy(() => import('./pages/Operators/Operators')) 
+const Methods = lazy(() => import('./pages/Methods/Methods'));
+const Operators = lazy(() => import('./pages/Operators/Operators'));
+
+const NotFound = () => <h2>404 - Page Not Found</h2>; 
+
 function App() {
   return (
     <div>
       <div className="logo">Web Dev Training...</div>
-          <Suspense fallback={<div className='loader'><DNA
-  visible={true}
-  height="350"
-  width="350"
-  ariaLabel="dna-loading"
-  wrapperStyle={{}}
-  wrapperClass="dna-wrapper"
-  /></div>}>
-            <Routes>
+      <Suspense fallback={<div className='loader'><DNA visible={true} height="350" width="350" ariaLabel="dna-loading" wrapperStyle={{}} wrapperClass="dna-wrapper" /></div>}>
+        <Routes>
           <Route path="/" element={<HomePage />} />
-              <Route path="/test" element={<Test />} />
-          
-              <Route path="/tags" element={<Tags />} />
-              <Route path="/text" element={<Text />} />
-          
-              <Route path="/grid" element={<Grid />} />
-              <Route path="/img" element={<Img />} />
-              <Route path="/shapes" element={<Shapes />} />
-              <Route path="/animations" element={<Animations />} />
-              <Route path="/galleries" element={<Galleries />} />
-          
-              <Route path="/basic-statements" element={<BasicStatements />} />
-              <Route path="/methods" element={<Methods />} />
-              <Route path="/operators" element={<Operators />} />
-              <Route path="/functions" element={<Functions />} />
-              <Route path="/branch-statements" element={<BranchStatements />} />
-              <Route path="/cycles" element={<Cycles />} />
-              <Route path="/arrays" element={<Arrays />} />
-              <Route path="/objects" element={<Objects />} />
-              <Route path="/calc" element={<Calc />} />
-            </Routes>
-          </Suspense>
+          <Route path="/test" element={<Test />} />
+          <Route path="/tags" element={<Tags />} />
+          <Route path="/text" element={<Text />} />
+          <Route path="/grid" element={<Grid />} />
+          <Route path="/img" element={<Img />} />
+          <Route path="/shapes" element={<Shapes />} />
+          <Route path="/animations" element={<Animations />} />
+          <Route path="/galleries" element={<Galleries />} />
+          <Route path="/basic-statements" element={<BasicStatements />} />
+          <Route path="/methods" element={<Methods />} />
+          <Route path="/operators" element={<Operators />} />
+          <Route path="/functions" element={<Functions />} />
+          <Route path="/branch-statements" element={<BranchStatements />} />
+          <Route path="/cycles" element={<Cycles />} />
+          <Route path="/arrays" element={<Arrays />} />
+          <Route path="/objects" element={<Objects />} />
+          <Route path="/calc" element={<Calc />} />
+          <Route path="*" element={<NotFound />} /> 
+        </Routes>
+      </Suspense>
     </div>
   );
 }
--- src/pages/Galleries/Galleries.tsx
+++ src/pages/Galleries/Galleries.tsx
@@ -1,14 +1,54 @@
-import GoHomeBtn from '../../components/GoHomeBtn/GoHomeBtn'; 
+import React from 'react';
+import GoHomeBtn from '../../components/GoHomeBtn/GoHomeBtn';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import s from './Galleries.module.css';
 
-const Galleries:React.FC = () => {
+const images = [
+  {
+    id: 1,
+    url: '/img/chapter-two.jpg',
+    title: 'Chapter two'
+  },
+  {
+    id: 2,
+    url: '/img/coffee.jpg',
+    title: 'Coffee'
+  },
+  {
+    id: 3,
+    url: '/img/guitar.jpg',
+    title: 'Guitar'
+  }
+];
+
+interface Image {
+  id: number;
+  url: string;
+  title: string;
+}
+
+const Galleries: React.FC = () => {
   return (
     <div className='container'>
       <GoHomeBtn />
-    <div>
-      <h2 className="title">Masonry Layout</h2>
+      <div>
+        <h2 className="title">Masonry Layout</h2>
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 1023: 4, 1440: 5 }}>
+          <Masonry className={s.masonryGrid}>
+            {images.map((image: Image) => (
+              <div key={image.id} className={s.imageWrapper}>
+                <img
+                  src={image.url} 
+                  alt={image.title}
+                  className={s.image}
+                />
+              </div>
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Galleries
+export default Galleries;
