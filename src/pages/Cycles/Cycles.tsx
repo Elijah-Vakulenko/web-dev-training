fa7cd9dbe010b3400683342616cba6cd9ebd50b3
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-10-25 22:54:03 +0100

    +

--- src/pages/Cycles/Cycles.tsx
+++ src/pages/Cycles/Cycles.tsx
@@ -1,9 +1,52 @@
-import GoHomeBtn from '../../components/GoHomeBtn/GoHomeBtn';
 
-const Cycles:React.FC = () => {
+import GoHomeBtn from '../../components/GoHomeBtn/GoHomeBtn';
+import Task from '../../components/Task/Task';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+const forCycle = `for( let i = 0; i >= 10; i += 0 ){
+console.log('simple counter')
+}`;
+
+
+const Cycles:React.FC = () => { 
   return (
     <div className='container'>
-      <GoHomeBtn />Objects</div>
+      <GoHomeBtn />
+      <div>
+        <h2 className='title'>Цикл for</h2>
+     <p className='description'>
+          Використовуємо, коли відома кількість повторень циклу.<br/>
+          Структура: for( 1 ; 2 ; 3 )
+
+          
+     </p>
+<ul>
+  <li>1. Початкове значення змінної - зазвичай назва змінної "i" тобто ітерація</li>
+         <li> 2. Умова до якої буде виконуватися цикл / кінцеве значення  - результат умови це завжди булеве значення true / false</li>
+          <li>3. Пост-умова - в пост-умові замість i+=1 використовуємо інкремент чи дикремент i++ i--</li>
+          <li>В фігурних дужках прописуємо логіку того що має вібуватися за кожної ітерації</li>
+        </ul>
+        <SyntaxHighlighter language="jsx" style={dracula}>
+           {forCycle}
+          </SyntaxHighlighter>
+      </div>
+       <div>
+        <h2 className='title'>while(){}</h2>
+    
+      </div>
+      <div>
+      <h2 className='title'>do{}while()</h2>
+    
+      </div>
+      <div>
+      <h2 className='title'>Задачі на тренування циклів</h2>
+    <Task problem='' solution={``}></Task>
+    <Task problem='' solution={``}></Task>
+    <Task problem='' solution={``}></Task>
+      </div>
+
+    </div>
   )
 }
 
--- src/pages/Methods/Methods.tsx
+++ src/pages/Methods/Methods.tsx
@@ -210,6 +210,45 @@ const Methods: React.FC = () => {
   if (!link.endsWith('/')) { //← Якщо посилання НЕ закінчується слешем, тоді
     link += '/'; // ← за допомогою канкатенації додаємо до змінної слеш.
   } else {console.log('no slash needed')}`}></Task>
+        
+          <Task
+            problem='Форматування посилання (includes та логічне «І»)
+ : Напиши скрипт який перевіряє чи закінчується значення
+  змінної link символом /. Якщо ні, додай до кінця
+  значення link цей символ, але тільки в тому випадку,
+  якщо в link є підрядок "my-site".
+  Використовуй конструкцію if...else або тернарний оператор.'
+          solution={`let url = 'https://my-site.com/about';
+
+if (!url.endsWith('/') && url.includes('my-site')) {
+ url += '/';
+}
+console.log(url);`}></Task>
+          <Task
+            problem=''
+          solution={`const blacklistedWord1 = 'spam';
+const blacklistedWord2 = 'sale';
+
+const message =
+  "Hello, I'm Prince Abdul, this is not spam, I'm offering you a million!";
+const string2 = "Biggest SALE this week, don't miss out!";
+const string3 = '#fatlivesmatter advertising campaign';
+
+const includesBanWord1 = string3.toLowerCase().includes(blacklistedWord1);
+const includesBanWord2 = string3.toLowerCase().includes(blacklistedWord2);
+const includesBanWords = includesBanWord1 || includesBanWord2;
+
+if (includesBanWords) {
+  console.log('Аяйяй');
+} else {
+  console.log(message);
+}`}></Task>
+          <Task
+            problem=''
+          solution={``}></Task>
+          <Task
+            problem=''
+          solution={``}></Task>
         </div>
 
      </div>
