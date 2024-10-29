e7309685c09630074390d13cef603e8262afadb2
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-10-29 23:25:42 +0000

    +

--- src/pages/Methods/Methods.tsx
+++ src/pages/Methods/Methods.tsx
@@ -154,8 +154,8 @@ const pad = ``
 const Methods: React.FC = () => {
 
   const startConverter = () => { 
-    const userNumber = prompt('Введіть своє число').trim();
-    const userNumber = userInput !== null ? Number(userInput) : NaN;
+    const userInput = prompt('Введіть своє число')?.trim(); 
+const userNumber = userInput !== null ? Number(userInput) : NaN; 
   console.log(userNumber);
 
   const hours = String(Math.floor(userNumber / 60)).padStart(2, '0');
